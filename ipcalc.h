157b21c95affab76fe0888d5f833c4e09b713643
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-10-30 10:20:22 +0100

    corrected macros when geoip is disabled

--- ipcalc.h
+++ ipcalc.h
@@ -3,8 +3,8 @@ void geo_ipv4_lookup(struct in_addr ip, char **country, char **ccode, char **cit
 void geo_ipv6_lookup(struct in6_addr *ip, char **country, char **ccode, char **city, char **coord);
 int geo_setup(void);
 #else
-# define geo_ipv4_lookup(x,y,z,w)
-# define geo_ipv6_lookup(x,y,z, w)
+# define geo_ipv4_lookup(x,y,z,w,a)
+# define geo_ipv6_lookup(x,y,z,w,a)
 # define geo_setup() -1
 #endif
 
